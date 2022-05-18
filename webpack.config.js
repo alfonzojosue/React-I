b57f5a7ed4851f6bef94601507d07e26df090cd1
Josue Alfonzo <16-00436@usb.ve>
2022-05-18 10:06:56 -0400

    Configuracion de webpack y babel para el proyecto

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // extensiones en las cuales actuará babel
+                exclude: /node_modules/, // siempre excluir node modules 
+                use: { // indicamos el loader
+                    loader: 'babel-loader' // babel 
+                }
+            },
+            {
+                test: /\.html$/, // extensiones html
+                use: [
+                    {
+                        loader: 'html-loader' // loader a usar
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [ // plugins 
+        new HtmlWebpackPlugin({ // instanciamos el plugin para html 
+            template: './public/index.html', // archivo raíz a transformar
+            filename: './index.html' // el archivo resultante
+        })
+    ]
+}
