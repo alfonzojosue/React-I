44b01f262b0609f15c494ada360cf388c3835f3d
Josue Alfonzo <16-00436@usb.ve>
2022-06-01 11:26:05 -0400

    Hooks

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProductItem from '@components/ProductItem';
+import useGetProducts from '../hooks/useGetProduct';
 import '@styles/ProductList.scss';
-import axios from 'axios';
+
+
 
 const API = 'https://api.escuelajs.co/api/v1/products?limit=15&offset=15';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async() => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
