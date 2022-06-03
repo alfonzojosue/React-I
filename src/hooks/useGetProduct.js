06a0585d555e34221b26f611d53ac1ddc9a2e8e2
Josue Alfonzo <16-00436@usb.ve>
2022-06-03 09:40:35 -0400

    Arreglando algunos errores de compatibilidad con react 18 y useRef

--- src/hooks/useGetProduct.js
+++ src/hooks/useGetProduct.js
@@ -4,12 +4,18 @@ import axios from 'axios';
 const useGetProducts = (API) => {
     const [products, setProducts] = useState([]);
 
-	useEffect(async() => {
+	useEffect(() => {
+		async function fetchMyAPI() {
 		const response = await axios(API);
 		setProducts(response.data);
-	}, [])
+	}
+	fetchMyAPI()
+}, [])
 
 return products;
 }
+
+
+
 
 export default useGetProducts;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from '@routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOMClient.createRoot(document.getElementById("app"));
+root.render(
+
+<React.StrictMode>
+    <App />
+</React.StrictMode>
+
+);
+
+reportWebVitals();
+
